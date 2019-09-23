2f2691385e5f898d0131d0012bc7e30294813e19
Orman, Clayton <CDO8081@yum.com>
2019-09-23 15:25:08 -0400

    Add bash file to install yarn and build static site

---
+++ deploy/deploy.sh
@@ -0,0 +1,9 @@
+cd ../
+
+echo "Installing dependencies for blog application"
+
+yarn --version
+
+echo "Building static site"
+
+gatsby build
--- deploy/server.js
+++ deploy/server.js
@@ -1,9 +1,25 @@
 const express = require("express")
 const app = express()
+const childProcess = require("child_process")
 const basePathPrefix = `/webhooks`
+const githubUserName = `thefrugaldev`
 
-app.get(`${basePathPrefix}/github`, (req, res) => {
-  res.send("Deploying blog application")
+app.post(`${basePathPrefix}/github/deploy`, (req, res) => {
+  let sender = req.body.sender
+  let branch = req.body.ref
+
+  if (branch.indexOf("master") > -1 && sender.login == githubUserName)
+    deploy(res)
 })
+
+function deploy(res) {
+  childProcess.exec(`./deploy.sh`, function(err, stdout, stderr) {
+    if (err) {
+      console.error(err)
+      return res.send(500)
+    }
+    res.send(200)
+  })
+}
 
 app.listen(8080, () => console.log("Starting deploy server"))
