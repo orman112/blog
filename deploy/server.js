a2a0fea3f68282abd84a96c9bb42266f0207bd4e
Orman, Clayton <CDO8081@yum.com>
2019-09-23 14:53:07 -0400

    Update server to listen on 8080

--- deploy/server.js
+++ deploy/server.js
@@ -1,3 +1,4 @@
 const express = require("express")
 const app = express()
 app.get("/webhook/github", (req, res) => res.send("Deploying app..."))
+app.listen(8080, () => console.log("Starting deploy server"))
