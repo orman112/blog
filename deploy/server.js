5b1113f5e65f6d87c5ca88d193c35f76c3889c06
Orman, Clayton <CDO8081@yum.com>
2019-09-23 16:55:15 -0400

    Update logs

--- deploy/server.js
+++ deploy/server.js
@@ -23,7 +23,11 @@ app.post(`${basePathPrefix}/github/deploy`, (req, res) => {
 })
 
 function deploy(res) {
-  childProcess.exec("./deploy.sh", function(err, stdout, stderr) {
+  childProcess.exec("/var/www/blog/deploy/deploy.sh", function(
+    err,
+    stdout,
+    stderr
+  ) {
     if (err) {
       return res.status(500).send(err)
     }
