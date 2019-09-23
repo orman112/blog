1a02b3a70a817366085469e5feaf8aa3f4ddf3c1
Orman, Clayton <CDO8081@yum.com>
2019-09-23 15:10:12 -0400

    Update deploy endpoint for testing

--- deploy/server.js
+++ deploy/server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-app.get("/webhook/github", (req, res) => {
+app.get("/webhooks/github", (req, res) => {
   res.send("Deploying blog application")
 })
 
