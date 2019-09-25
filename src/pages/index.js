95be67af42e5c668c5c698a6baea5f522dae2249
Orman, Clayton <CDO8081@yum.com>
2019-09-25 16:40:16 -0400

    Update main page text for CI/CD testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,8 +18,8 @@ class BlogIndex extends React.Component {
         <Title text="The Frugal Dev" />
         <p className="alert">
           Welcome to my blog! My name is <Link to="/about">Clayton</Link> and I
-          enjoy learning about new and interesting topics, specifically in the
-          technology and personal finance fields. I also enjoy teaching and
+          enjoy learning all about new and interesting topics, specifically in
+          the technology and personal finance fields. I also enjoy teaching and
           discussing these topics, as I feel this is truly the only way to fully
           understand a subject. So, I created this blog to do just that! I hope
           you enjoy reading some of my content as I take this journey to expand
