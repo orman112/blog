335836e3918fef5853184d88b766081f5a21686d
Clayton Orman <corman.dev@gmail.com>
2019-10-01 20:45:51 -0400

    Link to blog from homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,11 +20,11 @@ class BlogIndex extends React.Component {
           enjoy learning all about new and interesting topics, specifically in
           the technology and personal finance fields. I also enjoy teaching and
           discussing these topics, as I feel this is truly the only way to fully
-          understand a subject. So, I created this blog to do just that! I hope
-          you enjoy reading some of my content as I take this journey to expand
-          my knowledge on certain topics, while also rambling about a few other
-          areas that interest me as well. I also hope you find them useful and
-          learn something along the way!
+          understand a subject. So, I created this <Link to="/blog">blog</Link>{" "}
+          to do just that! I hope you enjoy reading some of my content as I take
+          this journey to expand my knowledge on certain topics, while also
+          rambling about a few other areas that interest me as well. I also hope
+          you find them useful and learn something along the way!
         </p>
         <div className={styles.homeImage}></div>
       </Layout>
